ddb3ad32b39d2d0d52fb3bd15d7d6cee6b330bc3
Trịnh Trịnh <trinhkimngan2k6@gmail.com>
2025-02-18 22:42:49 +0700

    Add files via upload

---
+++ bai1_KDL.cpp
@@ -0,0 +1,24 @@
+﻿/*	Tính toán giá trị của biểu thức A(x) = x ^ 3 + 3x ^ 2 + x + 1 
+	Với giá trị của x được nhập từ bàn phím, tính và in ra giá trị của biểu thức trên		*/
+
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+
+int main() {
+	int x;
+	long long a;
+
+	cin >> x;
+
+	//Procesing
+	a = pow(x, 3) + 3 * pow(x, 2) + x + 1;
+
+	//Gia tri cua bieu thuc A(x)
+	cout << a << endl;
+
+	system("pause");
+
+	return 0;
+}
---
+++ bai2_KDL.cpp
@@ -0,0 +1,22 @@
+﻿//Cho ba số nguyên a , b và c, hãy tính S = a ( b + c ) + b ( a + c ).
+#include<iostream>
+using namespace std;
+
+
+int main() {
+	long long a, b, c;
+	long long s;
+
+	//Input:Nhap 3 so nguyen a,b,c
+	cin >> a >> b >> c;
+
+	//Processing
+	s = a * (b + c) + b * (a + c);
+
+	//Output:gia tri cua bieu thuc S
+	cout << s << endl;
+
+	system("pause");
+
+	return 0;
+}
