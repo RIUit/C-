8a9bb80e4e6f3ddd5e1a454162639555bd205007
Trịnh Trịnh <trinhkimngan2k6@gmail.com>
2025-02-19 21:55:46 +0700

    Add files via upload

---
+++ bai10_KDL.cpp
@@ -0,0 +1,17 @@
+﻿/*	Cho 3 cạnh a, b, c là độ dài 3 cạnh của tam giác, kiểm tra xem a, b, c có 
+	thể tạo thành một tam giác hợp lệ hay không?								*/
+#include<iostream>
+using namespace std;
+
+
+int main() {
+	int a, b, c;
+	
+	cin >> a >> b >> c;
+
+	if (a + b > c && b + c > a && a + c > b) {
+		cout << "YES\n";
+	}
+	else
+		cout << "NO\n";
+}
