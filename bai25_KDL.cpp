080922ac3d52ddb2436cb512e767dde7f202efb0
Trịnh Trịnh <trinhkimngan2k6@gmail.com>
2025-02-19 21:02:04 +0700

    Add files via upload

---
+++ bai25_KDL.cpp
@@ -0,0 +1,19 @@
+/**/
+#include<iostream>
+using namespace std;
+
+
+int main() {
+	int n, u1, d;
+	long long un,sn;
+
+	//Input:so n,u1,d
+	cin >> n >> u1 >> d;
+
+	//Processing
+	un = u1 + 1ll*(n - 1) * d;
+	sn = n * (u1 + un) / 2;
+
+	//Output
+	cout << sn << endl;
+}
