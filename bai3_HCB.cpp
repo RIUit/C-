d65b48a0907324ad82534be43d2ec766edcdbb38
Trịnh Trịnh <trinhkimngan2k6@gmail.com>
2025-02-25 22:36:17 +0700

    Add files via upload

---
+++ bai3_HCB.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+
+int solve(int n) {
+	return 28 * n;
+}
+int main() {
+	int n;
+
+	cin >> n;
+
+	cout << solve(n) << endl;
+
+
+	system("pause");
+	
+	return 0;
+}
