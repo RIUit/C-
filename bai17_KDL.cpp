5815de0da378f9095018d2a036ffd78de2a705b5
Trịnh Trịnh <trinhkimngan2k6@gmail.com>
2025-02-25 21:08:50 +0700

    Add files via upload

---
+++ bai17_KDL.cpp
@@ -0,0 +1,34 @@
+﻿/*	Cho một kí tự, bạn hãy kiểm tra kí tự nhập vào là chữ cái in hoa, in thường, chữ số hay kí tự
+	đặc biệt (các kí tự không phải là chữ cái và chữ số)
+
+	=> Nếu c là chữ cái in hoa in ra "UPPER". Nếu c là chữ cái in thường in ra "LOWER". Nếu c là chữ
+	số in ra "DIGIT". Nếu c là kí tự đặc biệt in ra "SPECIAL".
+
+
+*/
+#include<iostream>
+using namespace std;
+
+
+int main() {
+	char c;
+	cin >> c;
+
+	if (c >= 'a' && c <= 'z')
+		cout << "LOWER" << endl;
+
+	else if (c >= 'A' && c <= 'Z')
+		cout << "UPPER" << endl;
+
+	else if (c >= '0' && c <= '9')
+		cout << "DIGIT" << endl;
+
+	else
+		cout << "SPECIAL" << endl;
+
+
+
+	system("pause");
+
+	return 0;
+}
