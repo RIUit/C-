0655041f11a0f51fe2b448295cdc510867e942f8
Trịnh Trịnh <trinhkimngan2k6@gmail.com>
2025-03-12 23:47:39 +0700

    Add files via upload

---
+++ bai8_VL.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<iomanip>
+#include<cmath>
+using namespace std;
+using ll=long long;
+
+int main(){
+	ll n;
+	cin>>n;
+	
+	for(ll i=1;i<=sqrt(n);i++){
+			cout<<i*i<<" ";
+	}
+	
+	
+
+	
+	return 0;
+}
