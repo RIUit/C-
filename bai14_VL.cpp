fdf976a9d40778babbd3015b3226e513524b0614
Trịnh Trịnh <trinhkimngan2k6@gmail.com>
2025-03-19 00:17:10 +0700

    Add files via upload

---
+++ bai14_VL.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+
+
+int main(){
+	int n;
+	cin>>n;
+	
+	long long tong=0;
+	for(int i=1;i<=n;i++){
+		tong+=pow(i,3);
+	}
+	cout<<tong<<endl;
+	
+	return 0;
+}
