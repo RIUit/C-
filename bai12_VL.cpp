812e54b5beac651ecb9e2dfba2dd39763b301916
Trịnh Trịnh <trinhkimngan2k6@gmail.com>
2025-03-19 00:12:18 +0700

    Add files via upload

---
+++ bai12_VL.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+
+
+int main(){
+	int n;
+	cin>>n;
+	
+	long long tong=0;
+	for(int i=1;i<=n;i++){
+		tong+=i*2;
+	}
+	cout<<tong<<endl;
+	
+	return 0;
+}
