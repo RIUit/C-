e125e15395d1a86633c87e49c0bb47b6e99b25f1
Trịnh Trịnh <trinhkimngan2k6@gmail.com>
2025-03-18 17:17:29 +0700

    Add files via upload

--- bai2_VLCB.cpp
+++ bai2_VLCB.cpp
@@ -3,23 +3,49 @@
 using namespace std;
 
 
-int main() {
+int main(){
 	int n;
-
-	cin >> n;
-
-	for (int i = 0; i <= n; i += 3)
-		if (i % 3 == 0)
-			cout << i << " ";
-	cout << endl;
-
-	for (int i = 0; i <= n; i += 1)
-		if (pow(2, i) <= n)
-			cout << pow(2, i) << " ";
-
-
-
-	system("pause");
-
+	cin>>n;
+	
+	//1
+	for(int i=0;i<=n;i+=3)
+		cout<<i<<" ";
+		
+	cout<<endl;
+	
+	//2
+	for(int i=0;i<=n;i++){
+		if(pow(2,i)<=n)
+			cout<<pow(2,i)<<" ";
+	}
+	cout<<endl;
+	
+	//3
+	for(int i=n;;i++){
+		if(i%17==0){
+			cout<<i<<endl;
+			break;
+		}	
+	}
+	
+	//4
+	for(int i=n-1;;i--)
+		if(i%7==0){
+			cout<<i<<endl;
+			break;
+		}
+	
+	//5
+	int dem=0;
+	for(int i=1;i<=n;i=i+dem){
+		cout<<i<<" ";
+		dem++;
+	}
+	cout<<endl;
+			
+		
+		
+	
+	
 	return 0;
-}
+} 
