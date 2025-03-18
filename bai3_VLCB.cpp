04a723424dead5019250e484f368b73313d8dfad
Trịnh Trịnh <trinhkimngan2k6@gmail.com>
2025-03-18 17:17:46 +0700

    Add files via upload

--- bai3_VLCB.cpp
+++ bai3_VLCB.cpp
@@ -2,52 +2,60 @@
 using namespace std;
 
 
-int main() {
+int main(){
 	int n;
-	cin >> n;
-
-	for (int i = 1; i <= n; i++)
-		cout << i << " ";
-
-	cout << endl;
-
-	int a = n;
-	while (a>=0) {
-		cout << a << " ";
-		a--;
+	cin>>n;
+	
+	//1
+	for(int i=1;i<=n;i++){
+		cout<<i<<" ";
 	}
-	cout << endl;
-
-	for (int i = 0; i <= n; i++) {
-		if (i % 2 == 0)
-			cout << i << " ";
+	cout<<endl;
+	
+	//2
+	int tam=n;
+	while(tam>=0){
+		cout<<tam--<<" ";
 	}
-	cout << endl;
-
-	int i = 0;
-	while (i <= n) {
-		if (i % 2 != 0)
-			cout << i << " ";
-		i++;
+	cout<<endl;
+	
+	//3
+	for(int i=0;i<=n;i++){
+		if(i%2==0)
+			cout<<i<<" ";
 	}
-	cout << endl;
-
-	for (int i = 0; i <= n; i++) {
-		if (i % 4 == 0)
-			cout << i << " ";
+	cout<<endl;
+	
+	//4
+	int i=0;
+	while(i<=n){
+	 	if(i%2!=0){
+	 		cout<<i<<" ";	
+		 }
+		i++;	
 	}
-	cout << endl;
-
-	for (int i = 0; i < n; i++) {
-		cout << (char)('a' + i) << " ";
+	cout<<endl;
+	
+	//5
+	for(int i=0;i<n;i+=4){
+		cout<<i<<" ";
 	}
-	cout << endl;
+	cout<<endl;
+	
+	//6
+	int k=0;
+	while(k<n){
+		cout<<(char)('a'+k)<<" ";
+		k++;
+	}
+	cout<<endl;
 
-	for (int i = 'z'-n+1; i <='z'; i++) {
-		cout << (char)i << " ";
+	//7
+	for(char i='z'-(n-1);i<='z';i++){
+		cout<<i<<" ";
 	}
 
-	system("pause");
-
+	
 	return 0;
+	
 }
