74d27dfb1413430b370b72fadd9953dd15ad16f7
Trịnh Trịnh <trinhkimngan2k6@gmail.com>
2025-03-29 22:39:11 +0700

    Add files via upload

---
+++ bai5_DQ.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+
+int giaiThua(int n){
+	if(n==0||n==1) return 1;
+	return n*giaiThua(n-1);
+}
+int main(){
+	int n;
+	cin>>n;
+	cout<<giaiThua(n)<<endl;
+	return 0;
+} 
