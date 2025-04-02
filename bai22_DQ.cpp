4e283233ed8ca6665daf5641ecbb22147d61e50f
Trịnh Trịnh <trinhkimngan2k6@gmail.com>
2025-04-02 22:13:11 +0700

    Add files via upload

---
+++ bai22_DQ.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+
+void daoMang(int*p,int l,int r){
+	if(l>r) return;
+	swap(p[l],p[r]);
+	daoMang(p,l+1,r-1);
+}
+int main(){
+	int n;
+	cin>>n;
+	
+	int*a= new int[n];
+	for(int i=0;i<n;i++){
+		cin>>a[i]; 
+	} 
+	for(int i=0;i<n;i++){
+		cout<<a[i]<<" "; 
+	}
+	cout<<endl;
+	daoMang(a,0,n-1);
+	for(int i=0;i<n;i++){
+		cout<<a[i]<<" "; 
+	}
+	delete[]a;
+	return 0;
+}
