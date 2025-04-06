a7c81c85c264f2bcfbe201630d6e9a2fea333583
Trịnh Trịnh <trinhkimngan2k6@gmail.com>
2025-04-06 22:13:54 +0700

    Add files via upload

---
+++ bai30_VL.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+	int n;
+	cin>>n;
+
+	for(int i=0;i<n;i++){
+		int x;cin>>x;
+		if(x%2==0)
+			cout<<"EVEN\n";
+		else
+			cout<<"ODD\n";
+	}
+
+	return 0;
+}
