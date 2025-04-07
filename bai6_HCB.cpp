53cf5f7b99dd30f06672df464ac624687ccc432b
Trịnh Trịnh <trinhkimngan2k6@gmail.com>
2025-04-07 23:26:30 +0700

    Add files via upload

---
+++ bai6_HCB.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+
+int sum_digit(long long n){
+	int tong=0;
+    while(n!=0){
+    	tong+=n%10;
+    	n/=10;
+	}
+	return tong;
+}
+
+int main(){
+    int n; cin >> n;
+    long long x;
+    for(int i = 1; i <= n; i++){
+        cin >> x;
+        cout << sum_digit(x) << " ";
+    }
+    return 0;
+}
+
