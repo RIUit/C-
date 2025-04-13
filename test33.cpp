9c081359f88b93ac7aed84df43e3564a1383ddc3
Trịnh Trịnh <trinhkimngan2k6@gmail.com>
2025-04-13 23:19:23 +0700

    Add files via upload

--- test33.cpp
+++ test33.cpp
@@ -1,55 +1,63 @@
 #include<iostream>
 using namespace std;
 
-
-void khoiTao(int **&p, int r, int c){
-	p = new int *[r];
-	for(int i = 0; i < r; i++){
-		p[i] = new int [c];
-	}
+const int MAX = 15;
+void input(){
+	
 }
-void giaiPhong(int **&p, int r, int c){
-	for(int i = 0; i < r; i++){
-		delete [] p[i];
-	}
-	delete [] p;
-}
-int chap(int n, int k){
-	if(n == k || k == 0)
-		return 1;
-	return chap(n-1,k) + chap(n-1,k-1);
-}
-void Pascal(int **p,int h){
-	for(int i = 0; i < h; i++){
-		for(int j = 0; j <= i; j++){
-			p[i][j] = chap(i,j);
+void khoiTao(int a[MAX][MAX],int r,int c){
+	for(int i=0;i<r;i++){
+		for(int j=0;j<c;j++){
+			cin>>a[i][j];
 		}
 	}
 }
-void xuat(int **p, int h){
-	for(int i = 0; i < h; i++){
-		for(int j = 0; j <= i; j++){
-			cout << p[i][j] << " ";
+void xuatMang(const int a[MAX][MAX],int r,int c){
+	for(int i=0;i<r;i++){
+		for(int j=0;j<c;j++){
+			cout<<a[i][j]<<" ";
 		}
-		cout << endl;
+		cout<<endl;
+	}
+}
+void tongMT(int a[MAX][MAX],int b[MAX][MAX],int c[MAX][MAX],int n){
+	for(int i =0;i<n;i++){
+		for(int j=0;j<n;j++){
+			c[i][j]=a[i][j]+b[i][j];
+		}
+	}
+}
+void tichMT(int a[MAX][MAX],int b[MAX][MAX],int c[MAX][MAX],int n){
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++){
+			c[i][j] = 0;
+			for(int k = 0;k<n;k++){
+				c[i][j]=a[i][k]*b[k][j];
+			}
+		}
 	}
 }
 int main(){
-	int h;
-	cout << "Nhap h : ";
-	cin >> h;
+	int r, c;
+	int a[MAX][MAX];
+	int b[MAX][MAX];
+	int tong[MAX][MAX];
+	int tich[MAX][MAX];
+	do{
+		cout << "Nhap so hang va so cot : ";
+		cin >> r >> c;
+		if(r < 1 || r > MAX || c < 1 || c > MAX)
+			cout << "Nhap hang va cot phai lon hon 0 !\n";
+	}while(r < 1 || r > MAX || c < 1 || c > MAX);
 	
-	int **p = new int *[h];
-	for(int i = 0; i < h; i++){
-		p[i] = new int [h];
-	}
-	Pascal(p,h);
-	xuat(p,h);
+	khoiTao(a,r,c);
+	khoiTao(b,r,c);
+	tongMT(a,b,tong,r);
+	xuatMang(tong,r,c);
+	tichMT(a,b,tich,r);
 	
-	//Huy cap phat
-	for(int i = 0; i < h; i++){
-		delete [] p[i];
-	}
-	delete [] p;
+	
+	
+	
 	return 0;
 }
