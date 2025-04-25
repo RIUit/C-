5f2f96d07e0e8ad8c16a4b59a6fbc18dae59432b
Trịnh Trịnh <trinhkimngan2k6@gmail.com>
2025-04-25 22:16:28 +0700

    Add files via upload

---
+++ bai7_SetMapCB.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<map>
+using namespace std;
+
+
+int main(){
+	int n;
+	cin >> n;
+	
+	map<int, int> mp;
+	for(int i = 0; i < n; i++){
+		int x;cin >> x;
+		mp[x]++;
+	}
+			
+	for(auto it : mp){
+		cout << it.first << " " << it.second << endl;
+	}
+	
+
+	
+	
+	return 0;
+}	
